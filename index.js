c4daeb398bd586830e5799a1ecff0e291578d034
Eric <ericbyington33@gmail.com>
2022-11-19 20:44:11 -0500

    add new team member or finish

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const employee = require("./lib/employee");
 const manager = require("./lib/manager");
 const engineer = require("./lib/engineer");
 const intern = require("./lib/intern");
+const buildTeam =require("./src/writeHTML");
+
+const teamInfo = [];
 
 function managerInfo () {
     inquirer .prompt([
@@ -28,14 +31,111 @@ function managerInfo () {
             type: "input",
             message: "Please enter team manager's office number",
             name: "number"
-        }
+        },
+        {
+            type: "list",
+            message: "Would you like to add another team member or finish building?",
+            name: "teamAdd",
+            choices: [ "engineer", "intern", "finish build" ]
+        },
+    ])
+    .then((ans) => {
+        teamInfo.push(
+          new manager(
+            ans.name,
+            ans.id,
+            ans.email,
+            ans.number
+          )
+        )
+    console.log(teamInfo)
+    switch (ans.teamAdd) {
+        case "engineer":
+          newEngineer();
+          break;
+        case "intern":
+          nweIntern();
+          break;
+        case "finish build":
+          buildTeam(teamInfo);
+          break;
+      }
+    });
+}
+function newEngineer(){
+    inquirer .prompt([
+        {
+// engineer questions
+        },
         {
             type: "list",
             message: "Would you like to add another team member or finish building?",
             name: "teamAdd",
             choices: [ "engineer", "intern", "finish build" ]
             
-        }
-    ]);
+        },
+        
+    ])
+    .then((ans) => {
+        teamInfo.push(
+          new engineer(
+            ans.name,
+            ans.id,
+            ans.email,
+            ans.number
+          )
+        )
+    console.log(teamInfo)
+    switch (ans.teamAdd) {
+        case "engineer":
+          newEngineer();
+          break;
+        case "intern":
+          nweIntern();
+          break;
+        case "finish build":
+          buildTeam(teamInfo);
+          break;
+      }
+    });
 }
+
+function newIntern(){
+    inquirer .prompt([
+        {
+// intern questions
+        },
+        {
+            type: "list",
+            message: "Would you like to add another team member or finish building?",
+            name: "teamAdd",
+            choices: [ "engineer", "intern", "finish build" ]
+            
+        },
+    ])
+    .then((ans) => {
+        teamInfo.push(
+          new engineer(
+            ans.name,
+            ans.id,
+            ans.email,
+            ans.number
+          )
+        )
+    console.log(teamInfo)
+    switch (ans.teamAdd) {
+        case "engineer":
+          newEngineer();
+          break;
+        case "intern":
+          nweIntern();
+          break;
+        case "finish build":
+          buildTeam(teamInfo);
+          break;
+      }
+    });
+}
+
+module.exports = managerInfo;
 managerInfo();
